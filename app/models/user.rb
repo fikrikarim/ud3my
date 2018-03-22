64a511c1da64811264233ced245f4ab7fc766ed1
Fikri Karim <fkfikrikarim@gmail.com>
2018-03-22 19:35:04 +0700

    Add validation for User informations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :first_name, :last_name, :citizen_id, :role, presence: true
+
+  # validates_presence_of :first_name
+  # validates_presence_of :last_name
+  # validates_presence_of :citizen_id
+  # validates_presence_of :role
+  validates_presence_of :student_id, :if => :student?
+  validates_presence_of :instructor_id, :if => :instructor?
+  validates_presence_of :department_name, :if => :instructor?
+
+  def student?
+    role == "student"
+  end
+
+  def instructor?
+    role == "instructor"
+  end
 end
