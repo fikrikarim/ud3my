0c255f447899c4380aae8087389003b4ae800a60
Fikri Karim <fkfikrikarim@gmail.com>
2018-03-23 11:48:14 +0700

    Create User Index Page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.all
+    @users = User.where(role: 'student')
   end
 
   # GET /users/1
