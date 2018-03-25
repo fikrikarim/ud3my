ea51d73906f92701639b21f7176d22399b80af91
Fikri Karim <fkfikrikarim@gmail.com>
2018-03-25 20:28:19 +0700

    Add scenario "can filter users for by name, student ID and group name" on user_spec

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -32,5 +32,35 @@ feature 'User system' do
         expect(page).to have_content student2.student_id
         expect(page).to have_content student2.group.name
     end
+
+    scenario "can filter users for by name, student ID and group name" do
+        student1 = create(:student)
+        student2 = create(:student2)
+        group1 = create(:group)
+        group2 = create(:group2)
+        group1.users<<(student1)
+        group2.users<<(student2)
+        visit users_path
+        
+        fill_in 'q_search_cont', with: student1.first_name
+        find_button('Search').click
+        expect(page).to have_content student1.first_name
+        expect(page).not_to have_content student2.first_name
+        
+        fill_in 'q_search_cont', with: student1.last_name
+        find_button('Search').click
+        expect(page).to have_content student1.last_name
+        expect(page).not_to have_content student2.last_name
+        
+        fill_in 'q_search_cont', with: student1.student_id
+        find_button('Search').click
+        expect(page).to have_content student1.student_id
+        expect(page).not_to have_content student2.last_name
+        
+        fill_in 'q_search_cont', with: student1.group.name
+        find_button('Search').click
+        expect(page).to have_content student1.group.name
+    end
+
   end
 end
